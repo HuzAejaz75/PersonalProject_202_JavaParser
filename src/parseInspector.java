0684e67b6baa6e6ce70c747a3d166fdca8ba3e11
HuzAejaz75 <huzaifa.aejaz@sjsu.edu>
2017-03-11 02:12:14 -0800

    Add files via upload

---
+++ src/parseInspector.java
@@ -0,0 +1,13 @@
+
+public class parseInspector {
+
+	String input_path;
+	String output_path;
+	
+	public parseInspector()
+	{
+		this.input_path = input_path;
+		this.output_path = output_path;
+		
+	}
+}
