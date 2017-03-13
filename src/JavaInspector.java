5f8537001ce35e78f8db791c419b4507b3343ad1
HuzAejaz75 <huzaifa.aejaz@sjsu.edu>
2017-03-13 01:29:39 -0700

    Add files via upload

---
+++ src/JavaInspector.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+
+import japa.parser.ParseException;
+
+public class JavaInspector {
+	public static void main(String[] args) throws Exception {
+		parseInspector ParseInspector = new parseInspector("/Users/huzaifa.aejaz/Desktop/input/", "a");
+    	ParseInspector.parseComputer();
+		
+		//ParseInspector.parser_agent();
+		
+		// write your code here
+	    	//parseInspector ParseInspector = new parseInspector("/Users/huzaifa.aejaz/Desktop/input/", "a");
+	    	//ParseInspector.parser_agent();
+	    	
+	    }
+	
+}
