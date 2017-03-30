318e7e8ea81d9dfa084fff404999011f4f2eda90
HuzAejaz75 <huzaifa.aejaz@sjsu.edu>
2017-03-29 23:26:11 -0700

    Add files via upload

--- src/parseInspector.java
+++ src/parseInspector.java
@@ -1,6 +1,10 @@
-import java.io.*;
-import java.util.*;
-import java.lang.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 import japa.parser.ast.Node;
 import japa.parser.ast.body.ClassOrInterfaceDeclaration;
@@ -10,49 +14,52 @@ import japa.parser.JavaParser;
 import japa.parser.ParseException;
 import japa.parser.ast.CompilationUnit;
 
+import japa.parser.JavaParser;
+
+//import com.github.javaparser.JavaParser;
+
+import japa.parser.ast.CompilationUnit;
+
+//import com.github.javaparser.ast.CompilationUnit;
 
 public class parseInspector {
-	final String input_path;
-    final String output_file;
-    List<CompilationUnit> comp_unit_holder;
-    HashMap<String, Boolean> classOrInterface_map;
-
-    parseInspector(String input_path, String output_file) {
-        this.input_path = input_path;
-        this.output_file = input_path + "\\" + output_file + ".png";
-    }
-    public void parseComputer() throws Exception {
-    	comp_unit_holder = getCuArray(input_path);
-    	classOrInterface(comp_unit_holder);
-    	System.out.println("the initial code trasnforms to");
-    	for(CompilationUnit comp_unit: comp_unit_holder)
+	String input_path;
+	String output_path;
+	List<CompilationUnit> comp_unit_holder;
+	  HashMap<String, Boolean> classOrInterface_map;
+	public parseInspector(String input_path, String output_file)
+	{
+		this.input_path = input_path;
+		this.output_path = input_path + "\\" + output_file + ".png";
+		
+	}
+	
+	public void parser_agent() throws Exception
+	{
+		comp_unit_holder = returned_compiler(input_path);
+		for(CompilationUnit comp_unit: comp_unit_holder)
     	{
     		System.out.println(comp_unit);
+    		intermediate_code_generator(comp_unit);
     	}
-    }
-    
-    private void classOrInterface(List<CompilationUnit> comp_unit_array)
-    {
-    	for(CompilationUnit comp_unit : comp_unit_array)
-    	{
-    		   List<TypeDeclaration> typeHolder = comp_unit.getTypes();
-               for (Node n : typeHolder) {
-                   ClassOrInterfaceDeclaration coi_holder = (ClassOrInterfaceDeclaration) n;
-                   
-                   if(coi_holder.isInterface())
-                   {
-                	   classOrInterface_map.put(coi_holder.getName(), true);
-                   }
-                   else
-                   {
-                	   classOrInterface_map.put(coi_holder.getName(), false);
-                   }
-                   
-                 
-               }
-    	}
-    }
-    private List<CompilationUnit> getCuArray(String input_path)
+		classOrInterface(comp_unit_holder);
+		
+	}
+	
+	
+	//String UML_code_holder = null;
+	
+	private void intermediate_code_generator(CompilationUnit comp_parse_unit)
+	{
+		List<TypeDeclaration> TypeHolder = comp_parse_unit.getTypes();
+		
+		for(TypeDeclaration T : TypeHolder)
+		{
+			System.out.print(T.toString());
+		}
+	}
+	
+	private List<CompilationUnit> returned_compiler(String input_path)
             throws Exception {
         File files = new File(input_path);
         List<CompilationUnit> comp_unit_collection = new ArrayList<CompilationUnit>();
@@ -79,4 +86,32 @@ public class parseInspector {
         return comp_unit_collection;
     }
 	
+	
+	private void classOrInterface(List<CompilationUnit> comp_unit_array)
+    {
+    	for(CompilationUnit comp_unit : comp_unit_array)
+    	{
+    		   List<japa.parser.ast.body.TypeDeclaration> typeHolder = comp_unit.getTypes();
+               for (Node n : typeHolder) {
+                   ClassOrInterfaceDeclaration coi_holder = (ClassOrInterfaceDeclaration) n;
+                   
+                   if(coi_holder.isInterface())
+                   {
+                	   classOrInterface_map.put(coi_holder.getName(), true);
+                   }
+                   else
+                   {
+                	   classOrInterface_map.put(coi_holder.getName(), false);
+                   }
+                   
+                 
+               }
+    	}
+    }
+	
+	
+	
+	
 }
+
+	
