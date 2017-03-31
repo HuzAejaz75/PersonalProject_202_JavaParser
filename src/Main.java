4821878966d027be93ac6797b27a420f53aba8ab
HuzAejaz75 <huzaifa.aejaz@sjsu.edu>
2017-03-30 23:43:56 -0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.io.FileNotFoundException;
+
+
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    	parseInspector ParseInspector = new parseInspector();
-    	
-    }
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+    	parseInspector ParseInspector = new parseInspector("/Users/huzaifa.aejaz/Desktop/input/", "a");	
+    	//parseInspector.parser_agent();
+    	ParseInspector.parser_agent();
+	}
+
 }
