9e00c52a19d6ae237e473fd1ca9cfd77222b3b6f
HuzAejaz75 <huzaifa.aejaz@sjsu.edu>
2017-04-07 13:02:37 -0700

    Update JavaInspector.java

--- src/JavaInspector.java
+++ src/JavaInspector.java
@@ -7,12 +7,7 @@ public class JavaInspector {
 		parseInspector ParseInspector = new parseInspector("/Users/huzaifa.aejaz/Desktop/input/", "a");
     	ParseInspector.parseComputer();
 		
-		//ParseInspector.parser_agent();
 		
-		// write your code here
-	    	//parseInspector ParseInspector = new parseInspector("/Users/huzaifa.aejaz/Desktop/input/", "a");
-	    	//ParseInspector.parser_agent();
-	    	
 	    }
 	
 }
