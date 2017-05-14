e940ae67425558d9d85233bda8bcce189e720f64
HuzAejaz75 <huzaifa.aejaz@sjsu.edu>
2017-05-13 22:57:57 -0700

    Add files via upload

---
+++ src/Umlparser.java
@@ -0,0 +1,15 @@
+/**
+ * Created by huzaifa.aejaz on 2/18/17.
+ */
+public class Umlparser {
+
+    public static void main(String[] args) throws Exception {
+
+
+
+        ParseHub pe = new ParseHub("/Users/huzaifa.aejaz/Desktop/input/");
+        pe.controller();
+
+
+    }
+}
